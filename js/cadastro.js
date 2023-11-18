dfa56f0257bf1971271a9c8ea0c10f58d40690c8
Leandro Kodama <leandrokodama23@gmail.com>
2023-11-18 12:20:16 -0300

    Melhorias nas páginas finalizacao, perfil, cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,6 +1,14 @@
-document.getElementById('cadastroForm').addEventListener('submit', function(event){
+document.getElementById('cadastroForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Aqui você pode adicionar lógica para tratar os dados do formulário
-    alert("Cadastro realizado com sucesso!");
+    var senha = document.getElementById('senha').value;
+    var confirmarSenha = document.getElementById('confirmarSenha').value;
+
+    if (senha === confirmarSenha) {
+        // As senhas são iguais, você pode prosseguir com a lógica de cadastro
+        alert("Cadastro realizado com sucesso!");
+    } else {
+        // As senhas não são iguais, alerta o usuário
+        alert("As senhas não coincidem. Por favor, tente novamente.");
+    }
 });
