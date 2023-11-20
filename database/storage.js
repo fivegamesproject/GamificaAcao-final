7cdc7b1149d7503753f57f22e2ae9674a2f4523c
esqueleter <leo.alvesoliveira2000@gmail.com>
2023-11-19 22:28:30 -0300

    progress and header modification

---
+++ database/storage.js
@@ -0,0 +1,5 @@
+localStorage.setItem("username", username);
+localStorage.setItem("level", level);
+localStorage.setItem("actual_xp", actual_xp);
+localStorage.setItem("target_xp", target_xp);
+
--- js/header.js
+++ js/header.js
@@ -1,13 +1,44 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const progressBar = document.getElementById('custom-progress');
-    const progressLabel = progressBar.querySelector('.progress-label');
-    const remainingProgress = document.querySelector('.remaining-progress');
 
-   
-    const progressValue = 56; // Valor atual
-    const remainingValue = 100 - progressValue; // Valor que falta para 100%
+var username = localStorage.getItem("username");
+var level = localStorage.getItem("level");
+var actual_xp = localStorage.getItem("actual_xp");
+var target_xp = localStorage.getItem("target_xp");
 
+const progressBar = document.getElementById('custom-progress');
+const remainingProgress = document.querySelector('.remaining-progress');
+const usernameLabel = document.querySelector('.username');
+const levelLabel = document.querySelector('.level');
+
+if(username == 'undefined' || username == null){
+    localStorage.setItem("username", "Diego");
+    usernameLabel.textContent = username
+}
+
+if(level == 'undefined' || level == null){
+    localStorage.setItem("level", 1);
+    levelLabel.textContent = level
+}
+
+if(actual_xp == 'undefined' || actual_xp == null){
+    localStorage.setItem("actual_xp", 0); 
+
+    let progressValue = localStorage.getItem("actual_xp");
     progressBar.style.width = `${progressValue}%`;
-    progressLabel.textContent = `${progressValue}%`;
-    remainingProgress.textContent = remainingValue;
-});
+    remainingProgress.textContent = `${progressValue}%`;
+}
+
+if(target_xp == 'undefined' || target_xp == null){           
+    localStorage.setItem("target_xp", 100);    
+}
+
+
+window.addEventListener("localDataStorage", function () {
+    console.log("oi")
+}, false);
+
+
+
+
+
+
+
