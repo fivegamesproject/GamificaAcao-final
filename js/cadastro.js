a3c71dd3c7e30c7810947b71a8f7ff0e6c5573aa
Diegodas28 <diegoamorimsilva@gmail.com>
2023-11-27 01:50:07 -0300

    cadastro ranking

--- js/cadastro.js
+++ js/cadastro.js
@@ -12,3 +12,39 @@ document.getElementById('cadastroForm').addEventListener('submit', function(even
         alert("As senhas não coincidem. Por favor, tente novamente.");
     }
 });
+
+// -----------------------------------------------------------------------------------------------
+// cadastro.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('/path/to/cadastros.json')
+        .then(response => response.json())
+        .then(data => {
+            const cadastrosOrdenados = data.cadastros.sort((a, b) => b.ranking - a.ranking || b.xp - a.xp);
+
+       
+            exibirCadastros(cadastrosOrdenados);
+        })
+        .catch(error => console.error('Erro ao carregar dados:', error));
+});
+
+function exibirCadastros(cadastros) {
+ 
+    const cadastroContainer = document.querySelector('.cadastro-container');
+    cadastroContainer.innerHTML = '';
+
+    cadastros.forEach(cadastro => {
+        const cadastroDiv = document.createElement('div');
+        cadastroDiv.classList.add('cadastro');
+
+       
+        cadastroDiv.innerHTML = `
+            <h3>${cadastro.nome}</h3>
+            <p>Ranking: ${cadastro.ranking}</p>
+            <p>XP: ${cadastro.xp}</p>
+            <!-- Adicione mais informações conforme necessário -->
+        `;
+
+        cadastroContainer.appendChild(cadastroDiv);
+    });
+}
